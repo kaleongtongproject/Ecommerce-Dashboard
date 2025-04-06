f3b11cf48b9f86861f4a4244e15851a66b165e19
kaleongtongproject <kaleongtong@hotmail.com>
2025-04-06 08:36:21 -0700

    fixing path to mock data

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export async function fetchSalesData() {
   try {
-    const response = await fetch('data/salesData.json');
+    const response = await fetch('./data/salesData.json');
     if (!response.ok) throw new Error('Failed to fetch sales data');
     const data = await response.json();
     return data;
